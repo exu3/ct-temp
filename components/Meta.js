57a41c84780f40a15ee894fbbd9e28bf9e9c6a58
eilla1 <git@ella.cx>
2021-08-07 10:43:34 -0700

    theme config

--- components/Meta.js
+++ components/Meta.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 
 export default function Meta({ title }) {
-  const description = "This is a Next.js boilerplate.";
-  const keywords = "put some fun keywords here";
+  const description =
+    "Command Tech is a fiscally sponsored nonprofit utilising the power of technology to inspire female and nonbinary students.";
+  const keywords = "STEM, female, girls, nonbinary, coding, computer science";
   const author = "Ella";
   const twitter = "@ella";
   const image =
@@ -12,7 +13,7 @@ export default function Meta({ title }) {
       <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
-      <title>{title} | Next.js Template</title>
+      <title>{title} | Command Tech</title>
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
       <meta name="author" content={author} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: "#20166A",
+        white: "#ffffff",
+        black: "#000000",
+      },
+      fontFamily: {
+        montserrat: "Montserrat, sans-serif",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
